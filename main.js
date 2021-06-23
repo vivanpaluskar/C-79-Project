c3d2b84f2425c46583240e1e46c1479e93bbc8bc
vivanpaluskar <77099024+vivanpaluskar@users.noreply.github.com>
2021-06-23 03:32:06 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ for(var i=0;i<menu_list_array.length;i++){
     htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
 }
 htmldata=htmldata+'</ol>'
-document.getElementById("display_menu").innerhtml = htmldata;
+document.getElementById("display_menu").innerHTML = htmldata;
 }
 
 function add_item(){
 var htmldata;
-var item=document.getElementById("add_item").value;
+var item=document.getElementById("add_toppings").value;
 menu_list_array.push(item);
 menu_list_array.sort();
 htmldata="<section class='cards'>"
@@ -24,7 +24,7 @@ for(var i=0;i<menu_list_array.length;i++){
     + menu_list_array[i] + '</div>'
 }
 htmldata=htmldata+"</section>"
-document.getElementById("display_addmenu").innerHTML = htmldata;
+document.getElementById("display_addedmenu").innerHTML = htmldata;
 }
 
 function add_top(){
